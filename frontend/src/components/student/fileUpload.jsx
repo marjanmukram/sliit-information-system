c704a69bbedb1d0e227f17c4fb8617c1f2aeda45
Marjan Mukram <marjan.m@sl.aeturnum.com>
2019-06-19 18:45:37 +0530

    Basic foder structure

---
+++ frontend/src/components/student/fileUpload.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class FileUpload extends React.PureComponent{
+    render(){
+        
+        return(
+            <div>File upload component</div>
+        )
+    }
+}
+
+export default FileUpload;
