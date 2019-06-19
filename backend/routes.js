e0aa4026ea6a145e7bdf73594427b7889a090285
VithuJey <vithushjeytharma@gmail.com>
2019-06-19 19:02:58 +0530

    add minor admin changes

--- backend/routes.js
+++ backend/routes.js
@@ -3,8 +3,10 @@ var routes = express.Router();
 
 /** requires all classes that handles routes */ 
 var AdminRoute = require('./admin/AdminRoute');
+var InstructorRoute = require('./instructor/InstructorRoute');
 
 /** routes the request to the specified class */ 
 routes.use('/admin',AdminRoute);
+routes.use('/instructor',InstructorRoute);
 
 module.exports = routes;
