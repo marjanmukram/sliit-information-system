dca7d44f7f4b80fab7191d860d4ae195fa6ffe78
Marjan Mukram <marjan.m@sl.aeturnum.com>
2019-06-20 18:59:01 +0530

    Student submission form updated

---
+++ frontend/src/components/student/tests/fileUpload.test.js
@@ -0,0 +1,17 @@
+import React from "react"
+import FileUpload from "../fileUpload"
+import StudentSubmissionForm from '../submissionForm'
+import ShallowRenderer from "react-test-renderer/shallow"
+describe("<StudentSubmissionForm />", () => {
+  it("should render the StudentSubmissionForm and its children", () => {
+    const renderer = new ShallowRenderer()
+    renderer.render(
+      <StudentSubmissionForm>
+        <FileUpload />
+      </StudentSubmissionForm>
+    )
+    const result = renderer.getRenderOutput()
+    console.log(result)
+    expect(result.type).toBe("div")
+  })
+})
