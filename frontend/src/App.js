a1a245856231a982379264bd28f76f288d910558
Marjan Mukram <marjan.m@sl.aeturnum.com>
2019-06-20 16:58:46 +0530

    Basics done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './components/Login'
+import fileUpload from './components/student/fileUpload'
+import FileUpload from './components/student/fileUpload';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <FileUpload />
     </div>
   );
 }
