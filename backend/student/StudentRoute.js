c36bfed04d941bb7ee80d899ee7176363c3d0098
Marjan Mukram <marjan.m@sl.aeturnum.com>
2019-06-20 19:16:53 +0530

    Student routes comments removed

--- backend/student/StudentRoute.js
+++ backend/student/StudentRoute.js
@@ -50,21 +50,8 @@ router.delete('/delete/:id', (req,res) => {
             res.status(400).send({err})
         })
 })
-// Upload a assignment
 
-// router.post('/upload', (req, res, next) => {
-//     console.log(req);
-//     let imageFile = req.files.file;
-  
-//     imageFile.mv(`${__dirname}/public/${req.body.filename}.pdf`, function(err) {
-//       if (err) {
-//         return res.status(500).send(err);
-//       }
-  
-//       res.json({file: `public/${req.body.filename}.pdf`});
-//     });
-  
-//   })
+// Upload a assignment
 
 router.post('/upload/:id', (req, res) => {
     let imageFile = req.files.file;
@@ -75,12 +62,5 @@ router.post('/upload/:id', (req, res) => {
     
         res.json({file: `public/${req.body.filename}.pdf`});
       });
-    
-    // Student.findOneAndUpdate({'regNumber':req.params.id},{ $push: { assignments: req.files.filename || "path" } })
-    //     .then( path => {
-    //         res.status(200).send({"message":"Assignemnt updated", "data":path})
-    //     }).catch( err => {
-    //         res.status(400).send({err})
-    //     })
 })
 module.exports = router;
