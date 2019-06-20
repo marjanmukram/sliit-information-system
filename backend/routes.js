4034b0507b0350f185cc13bd377d2651d7211a69
kiru07 <kirushanthan007@gmail.com>
2019-06-20 15:15:00 -0800

    updated routes.js to include /exams and /submissions

--- backend/routes.js
+++ backend/routes.js
@@ -8,13 +8,17 @@ var InstructorRoute = require('./instructor/InstructorRoute');
 const AssignmentRoute = require("./Assignment/Assignment.route");
 const CourseRoute = require("./Course/Course.route");
 var StudentRoute = require('./student/StudentRoute')
+const ExamRoute = require('./Exam/Exam.route');
+const SubmissionRoute = require('./Submission/Submission.route');
 
-/** routes the request to the specified class */ 
+/** routes the request to the specified class */
 routes.use("/admin", AdminRoute);
-routes.use('/instructor',InstructorRoute);
+routes.use('/instructor', InstructorRoute);
 routes.use("/assignments", AssignmentRoute);
 routes.use("/courses", CourseRoute);
 routes.use('/student', StudentRoute);
+routes.use('/exams', ExamRoute);
+routes.use('/submissions', SubmissionRoute);
 
 
 module.exports = routes;
