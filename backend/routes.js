9f120a1569e898c72666957d8cae514e58aa73b0
kiru07 <kirushanthan007@gmail.com>
2019-06-20 11:21:37 -0800

    Added /assignments route to routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,14 @@
-var express = require('express'); 
+var express = require("express");
 var routes = express.Router();
 
-/** requires all classes that handles routes */ 
-var AdminRoute = require('./admin/AdminRoute');
+/** requires all classes that handles routes */
 
-/** routes the request to the specified class */ 
-routes.use('/admin',AdminRoute);
+var AdminRoute = require("./admin/AdminRoute");
+const AssignmentRoute = require("./Assignment/Assignment.route");
+
+/** routes the request to the specified class */
+
+routes.use("/admin", AdminRoute);
+routes.use("/assignments", AssignmentRoute);
 
 module.exports = routes;
