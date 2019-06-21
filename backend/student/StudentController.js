efb4e6c104767a35de04ab22306ce7d5bce861c5
kiru07 <kirushanthan007@gmail.com>
2019-06-21 11:38:22 -0800

    Updated StudentController and Submission.Controller

--- backend/student/StudentController.js
+++ backend/student/StudentController.js
@@ -1,6 +1,48 @@
 const Student = require("../models/Student");
 
 const StudentController = function() {
+  // Update Courses List of Student
+  this.updateCourseList = (id, courseId) => {
+    return new Promise((resolve, reject) => {
+      // Find student using id, update submission array then save.
+      Student.findById(id)
+        .then(student => {
+          if (student) {
+            student.courses.push(courseId);
+            student
+              .save()
+              .then(() => {
+                resolve({
+                  status: 200,
+                  confirmation: "Success",
+                  message: "Updated Courses List"
+                });
+              })
+              .catch(err => {
+                reject({
+                  status: 500,
+                  confirmation: "Fail",
+                  message: "Error: " + err
+                });
+              });
+          } else {
+            reject({
+              status: 404,
+              confirmation: "Fail",
+              message: "Student Not Found"
+            });
+          }
+        })
+        .catch(err => {
+          reject({
+            status: 500,
+            confirmation: "Fail",
+            message: "Error: " + err
+          });
+        });
+    });
+  };
+
   // Update submission list of Student
   this.updateSubmissionList = (id, submissionId) => {
     return new Promise((resolve, reject) => {
