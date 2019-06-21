83f7681acf0ac0ed1738f5d247fd76f2320cd248
Marjan Mukram <marjan.m@sl.aeturnum.com>
2019-06-21 20:15:51 +0530

    app.js reverted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './components/Login'
-import ViewCourses from './components/student/viewCourses'; 
+/* import ViewCourses from './components/student/viewCourses';  */
 
 function App() {
   return (
     <div className="App">
-      {/*  <Login/>  */}
-      <ViewCourses />
+        <Login/> 
+     {/*  <ViewCourses /> */}
     </div>
   );
 }
