e08289cb2a6f49a7d44c0122d03bc0e349d0a388
kiru07 <kirushanthan007@gmail.com>
2019-06-21 22:10:24 -0800

    resolved merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Homepage from './components/Homepage';
-<<<<<<< HEAD
-import Admin from './components/admin/CourseAdd'
-import AppRouter from './routers/AdminRouter'
-
-function App() {
-  return (
-    <Homepage/>
-=======
-import Admin from './components/admin/AdminHome'
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Homepage from "./components/Homepage";
+import Admin from "./components/admin/AdminHome";
 /* import ViewCourses from './components/student/viewCourses';  */
 
 function App() {
   return (
-    <Admin/>
-         /*  <ViewCourses /> */
->>>>>>> 42f47d436ead4cb0f217dd9a26a64ba3c05015c9
+    <Admin />
+    /*  <ViewCourses /> */
   );
 }
 
