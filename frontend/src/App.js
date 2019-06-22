34853dbac7ef211f604c59d81793bc782e1e7bbb
Rizan Mohomed <rmohomed@mitrai.com>
2019-06-22 09:56:08 +0530

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Login from './components/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 /* import ViewCourses from './components/student/viewCourses';  */
 
 function App() {
   return (
     <div className="App">
-        <Login/> 
-     {/*  <ViewCourses /> */}
+      <Login />
+      {/*  <ViewCourses /> */}
     </div>
   );
 }
